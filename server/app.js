91f429d6283159f86dae160d8ec853ce752b91a1
adan-enigmaticads <adan@enigmaticads.com>
2025-05-22 08:58:42 -0400

    updated app.use(session{}) in app.js

--- server/app.js
+++ server/app.js
@@ -39,7 +39,11 @@ app.use(session({
   store: sessionStore,
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 }
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    sameSite: 'none',
+    secure: true
+  }
 }));
 
 app.use('/api/auth', authRoutes);
